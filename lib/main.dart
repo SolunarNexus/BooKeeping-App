8c960759dbf4b64f802b34e4117fd83eadd132e1
Jakub Šušlík <suslikjakub@gmail.com>
2023-11-27 17:19:38 +0100

    added basic ioc setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:book_keeping/app_root.dart';
+import 'package:book_keeping/common/utility/ioc_container.dart';
 import 'package:book_keeping/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  IocContainer.setup();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
