2c8beb8575fe3ebbfc279d010be47c52382ff82b
Jakub Šušlík <suslikjakub@gmail.com>
2023-11-27 18:10:25 +0100

    reworked TopBar to extend AppBar

--- lib/common/widget/top_bar.dart
+++ lib/common/widget/top_bar.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class TopBar extends StatelessWidget {
-  final String title;
+class TopBar extends AppBar {
+  final String titleText;
   final bool includeActions;
 
-  const TopBar({super.key, required this.title, this.includeActions = true});
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: Text(
-        title,
-        style: GoogleFonts.quicksand(fontSize: 26, fontWeight: FontWeight.bold),
-      ),
-      centerTitle: true,
-      actions: includeActions
-          ? [
-              IconButton(
-                  onPressed: () => {}, icon: const Icon(Icons.notifications)),
-              IconButton(onPressed: () => {}, icon: const Icon(Icons.settings))
-            ]
-          : null,
-    );
-  }
+  TopBar({super.key, required this.titleText, this.includeActions = true})
+      : super(
+          title: Text(
+            titleText,
+            style: GoogleFonts.quicksand(
+                fontSize: 26, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          actions: includeActions
+              ? [
+                  IconButton(
+                      onPressed: () => {},
+                      icon: const Icon(Icons.notifications)),
+                  IconButton(
+                      onPressed: () => {}, icon: const Icon(Icons.settings))
+                ]
+              : null,
+        );
 }
