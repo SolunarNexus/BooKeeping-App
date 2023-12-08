961efb8a8494f0ae181ab9170557ecd585ebd7da
adamValent <adam.valent88@gmail.com>
2023-12-08 15:13:53 +0100

    Add preview book card

--- lib/home_page/widget/home_page.dart
+++ lib/home_page/widget/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:book_keeping/utils/top_bar.dart';
-import 'package:book_keeping/common/widget/book_list.dart';
-import 'package:book_keeping/common/widget/book_search_bar.dart';
+import 'package:book_keeping/common/widget/general_listview.dart';
+import 'package:book_keeping/common/widget/general_search_bar.dart';
 import 'package:book_keeping/common/widget/bottom_menu.dart';
 import 'package:book_keeping/common/widget/filter_buttons.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -12,15 +12,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bookCard = Card(
+      color: Colors.red[300],
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          children: [
+            // Will be replaced by real book cover image
+            const Icon(
+              Icons.image,
+              size: 100,
+              color: Colors.black,
+            ),
+            const Text(
+              'Book title',
+              style: TextStyle(color: Colors.black, fontSize: 19),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_horiz,
+                color: Colors.black,
+                size: 30.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
     return Scaffold(
       appBar: topBar(title: 'My library'),
-      bottomNavigationBar: BottomMenu(),
+      bottomNavigationBar: const BottomMenu(),
       body: Center(
         child: Column(
           children: [
             FilterButtons(labels: const ['Reading', 'Finished', 'Wishlist']),
-            const BookSearchBar(),
-            const BookList(),
+            const GeneralSearchBar(),
+            GeneralListView(items: List<Card>.generate(7, (_) => bookCard)),
             // TODO: Only for testing purposes, remove later
             TextButton(
               child: Text(FirebaseAuth.instance.currentUser!.email!),
