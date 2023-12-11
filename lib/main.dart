5a89b3ff2ef859d6b6848b2c07fa9cc5ee053a26
Jakub Šušlík <suslikjakub@gmail.com>
2023-12-11 17:09:45 +0100

    added firebase_messaging dependency, added notification permission prompt

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:book_keeping/app_root.dart';
 import 'package:book_keeping/common/utility/ioc_container.dart';
 import 'package:book_keeping/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
@@ -9,6 +10,15 @@ Future<void> main() async {
   IocContainer.setup();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
   );
   runApp(AppRoot());
 }
