ff1a1662305297ca8f50f163f7b4b5e4e7d69c32
Jakub Šušlík <suslikjakub@gmail.com>
2023-12-12 17:37:20 +0100

    added my book service

---
+++ lib/data_access/service/my_book_service.dart
@@ -0,0 +1,45 @@
+import 'package:book_keeping/common/utility/duplicate_data_exception.dart';
+import 'package:book_keeping/data_access/model/my_book.dart';
+import 'package:book_keeping/data_access/model/read_state.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MyBookService {
+  final _myBookCollection =
+      FirebaseFirestore.instance.collection('my_book').withConverter(
+    fromFirestore: (snapshot, options) {
+      final json = snapshot.data() ?? {};
+      json['id'] = snapshot.id;
+      return MyBook.fromJson(json);
+    },
+    toFirestore: (value, options) {
+      final json = value.toJson();
+      json.remove('id');
+      return json;
+    },
+  );
+
+  Stream<List<MyBook>> getStream(String userId) => _myBookCollection
+      .where("userId", isEqualTo: userId)
+      .snapshots()
+      .map((querySnapshot) =>
+          querySnapshot.docs.map((docSnapshot) => docSnapshot.data()).toList());
+
+  Future<void> create(String userId, String bookId) async {
+    if (await exists(userId, bookId)) {
+      throw DuplicateDataException(
+          "MyBook with userId: $userId and bookId: $bookId already exists");
+    }
+    final myBook =
+        MyBook(readState: ReadState.planToRead, userId: userId, bookId: bookId);
+    await _myBookCollection.add(myBook);
+  }
+
+  Future<bool> exists(String userId, String bookId) async {
+    final countSnapshot = await _myBookCollection
+        .where("userRef", isEqualTo: "/user/$userId")
+        .where("bookRef", isEqualTo: "/book/$bookId")
+        .count()
+        .get();
+    return countSnapshot.count > 0;
+  }
+}
