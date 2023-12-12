2b5e50c8e915e14a06aa7bda625489c610068200
Jakub Šušlík <suslikjakub@gmail.com>
2023-12-12 17:32:20 +0100

    added my book model

---
+++ lib/data_access/model/my_book.dart
@@ -0,0 +1,22 @@
+import 'package:book_keeping/data_access/model/read_state.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'my_book.g.dart';
+
+@JsonSerializable()
+class MyBook {
+  final String? id;
+  final ReadState readState;
+  final String userId;
+  final String bookId;
+
+  MyBook(
+      {this.id,
+      required this.readState,
+      required this.userId,
+      required this.bookId});
+
+  factory MyBook.fromJson(Map<String, dynamic> json) => _$MyBookFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MyBookToJson(this);
+}
---
+++ lib/data_access/model/my_book.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'my_book.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MyBook _$MyBookFromJson(Map<String, dynamic> json) => MyBook(
+      id: json['id'] as String?,
+      readState: $enumDecode(_$ReadStateEnumMap, json['readState']),
+      userId: json['userId'] as String,
+      bookId: json['bookId'] as String,
+    );
+
+Map<String, dynamic> _$MyBookToJson(MyBook instance) => <String, dynamic>{
+      'id': instance.id,
+      'readState': _$ReadStateEnumMap[instance.readState]!,
+      'userId': instance.userId,
+      'bookId': instance.bookId,
+    };
+
+const _$ReadStateEnumMap = {
+  ReadState.planToRead: 'planToRead',
+  ReadState.reading: 'reading',
+  ReadState.completed: 'completed',
+};
