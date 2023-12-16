ceac5af2c2e22da3b5e6e335984cd76843ce55d0
adamValent <adam.valent88@gmail.com>
2023-12-16 22:23:42 +0100

    Refactor ranking card

--- lib/ranking_page/widget/ranking_page.dart
+++ lib/ranking_page/widget/ranking_page.dart
@@ -1,8 +1,9 @@
 import 'package:book_keeping/common/widget/bottom_menu.dart';
-import 'package:book_keeping/common/widget/filter_buttons.dart';
+import 'package:book_keeping/common/widget/three_button_group.dart';
 import 'package:book_keeping/common/widget/general_listview.dart';
 import 'package:book_keeping/common/widget/general_search_bar.dart';
 import 'package:book_keeping/common/widget/top_bar.dart';
+import 'package:book_keeping/ranking_page/widget/ranking_card.dart';
 import 'package:flutter/material.dart';
 
 class RankingPage extends StatelessWidget {
@@ -10,66 +11,22 @@ class RankingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final rankingCard = Card(
-      color: Colors.red[300],
-      child: const SizedBox(
-        height: 100,
-        child: Row(
-          children: [
-            SizedBox(
-              width: 20,
-            ),
-            // Will be replaced by real avatar image
-            Text(
-              "1.",
-              style: TextStyle(fontSize: 30),
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            Text(
-              "Book title",
-              style: TextStyle(color: Colors.black, fontSize: 19),
-            ),
-            Spacer(),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    Icon(Icons.star),
-                    Icon(Icons.star),
-                    Icon(Icons.star),
-                    Icon(Icons.star),
-                    Padding(
-                      padding: EdgeInsets.only(right: 15),
-                      child: Icon(Icons.star_border),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("100,000 reviews"),
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-
     return Scaffold(
       appBar: TopBar(titleText: "Ranking", context: context),
       bottomNavigationBar: BottomMenu(),
       body: Center(
         child: Column(
           children: [
-            // TODO: needs more suitable name or another specialized widget for sorting
-            FilterButtons(
+            ThreeButtonGroup(
                 labels: const ["By reviews", "By ranking", "Combined"]),
             const GeneralSearchBar(),
-            GeneralListView(items: List<Card>.generate(7, (_) => rankingCard)),
+            GeneralListView(
+              items: List<Card>.generate(
+                7,
+                (index) => RankingCard(
+                    rank: index + 1, bookTitle: "Book Title", context: context),
+              ),
+            ),
           ],
         ),
       ),
