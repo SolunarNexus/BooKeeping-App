0276e0a27765e2d89def893a353f609639a6c357
adamValent <adam.valent88@gmail.com>
2023-12-16 22:23:59 +0100

    Refactor friend card a.k.a user card

--- lib/friends_page/widget/friends_page.dart
+++ lib/friends_page/widget/friends_page.dart
@@ -2,6 +2,7 @@ import 'package:book_keeping/common/widget/general_listview.dart';
 import 'package:book_keeping/common/widget/general_search_bar.dart';
 import 'package:book_keeping/common/widget/bottom_menu.dart';
 import 'package:book_keeping/common/widget/top_bar.dart';
+import 'package:book_keeping/friends_page/widget/user_card.dart';
 import 'package:flutter/material.dart';
 
 class FriendsPage extends StatelessWidget {
@@ -9,50 +10,17 @@ class FriendsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final friendCard = Card(
-      color: Colors.red[300],
-      child: SizedBox(
-        height: 100,
-        child: Row(
-          children: [
-            const SizedBox(
-              width: 10,
-            ),
-            // Will be replaced by real avatar image
-            const Icon(
-              Icons.account_circle,
-              size: 80,
-              color: Colors.black,
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            const Text(
-              "Bob Doe",
-              style: TextStyle(color: Colors.black, fontSize: 19),
-            ),
-            const Spacer(),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.more_horiz,
-                color: Colors.black,
-                size: 30.0,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-
     return Scaffold(
-      appBar: TopBar(titleText: "My friends", context: context),
+      appBar: TopBar(
+          titleText: "My friends", includeLeading: true, context: context),
       bottomNavigationBar: BottomMenu(),
       body: Center(
         child: Column(
           children: [
             const GeneralSearchBar(),
-            GeneralListView(items: List<Card>.generate(7, (_) => friendCard)),
+            GeneralListView(
+                items: List<Card>.generate(
+                    7, (_) => UserCard(userName: "Bob Doe", context: context))),
           ],
         ),
       ),
