3dd7d57d44fd6d31bdd47050124dd67ace62bbdc
adamValent <adam.valent88@gmail.com>
2023-12-16 22:24:59 +0100

    Refactor item count

--- lib/common/widget/general_listview.dart
+++ lib/common/widget/general_listview.dart
@@ -10,38 +10,9 @@ class GeneralListView extends StatelessWidget {
     return Expanded(
       child: ListView.builder(
         padding: const EdgeInsets.all(12.0),
-        itemCount: 7,
+        itemCount: _items.length,
         itemBuilder: (BuildContext context, int index) {
           return _items[index];
-          // return Card(
-          //   color: Colors.red[400],
-          //   child: SizedBox(
-          //     height: 100,
-          //     child: Row(
-          //       children: [
-          //         // Will be replaced by real book cover image
-          //         const Icon(
-          //           Icons.image,
-          //           size: 100,
-          //           color: Colors.black,
-          //         ),
-          //         const Text(
-          //           'Book title',
-          //           style: TextStyle(color: Colors.black, fontSize: 19),
-          //         ),
-          //         const Spacer(),
-          //         IconButton(
-          //           onPressed: () {},
-          //           icon: const Icon(
-          //             Icons.menu,
-          //             color: Colors.black,
-          //             size: 30.0,
-          //           ),
-          //         )
-          //       ],
-          //     ),
-          //   ),
-          // );
         },
       ),
     );
