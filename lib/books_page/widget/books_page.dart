362109aa992f1cbd7dc27cd74611eb7d405c7e70
adamValent <adam.valent88@gmail.com>
2023-12-16 22:23:08 +0100

    Refactor more detailed book card

--- lib/books_page/widget/books_page.dart
+++ lib/books_page/widget/books_page.dart
@@ -1,3 +1,4 @@
+import 'package:book_keeping/books_page/widget/book_card.dart';
 import 'package:book_keeping/common/widget/bottom_menu.dart';
 import 'package:book_keeping/common/widget/general_listview.dart';
 import 'package:book_keeping/common/widget/general_search_bar.dart';
@@ -9,52 +10,6 @@ class BooksPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bookCard = Card(
-      color: Colors.red[300],
-      child: const SizedBox(
-        height: 200,
-        child: Row(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(bottom: 10),
-                  child: Icon(
-                    Icons.image,
-                    size: 120,
-                    color: Colors.black,
-                  ),
-                ),
-                Icon(
-                  Icons.favorite_border,
-                  size: 30,
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 12.0),
-                  child: Text(
-                    "Book title",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                SizedBox(
-                  width: 220,
-                  child: Text(
-                    "Some interesting description of the book",
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-
     return Scaffold(
       appBar: TopBar(
         titleText: "Books",
@@ -65,7 +20,15 @@ class BooksPage extends StatelessWidget {
         child: Column(
           children: [
             const GeneralSearchBar(),
-            GeneralListView(items: List<Card>.generate(7, (_) => bookCard))
+            GeneralListView(
+              items: List<Card>.generate(
+                7,
+                (_) => BookCard(
+                    bookTitle: "Book Title",
+                    description: "Some interesting description of the book.",
+                    context: context),
+              ),
+            )
           ],
         ),
       ),
