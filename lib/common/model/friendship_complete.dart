e461b90c1391ab0b209f4365a1a81f92102a31fd
Jakub Šušlík <suslikjakub@gmail.com>
2023-12-29 17:03:32 +0100

    reworked stream in friendship_facade.dart to return list of friendship_complete.dart

---
+++ lib/common/model/friendship_complete.dart
@@ -0,0 +1,15 @@
+import 'package:book_keeping/common/model/friendship_state.dart';
+import 'package:book_keeping/data_access/model/user.dart';
+
+class FriendshipComplete {
+  final String? id;
+  final User user;
+  final User otherUser;
+  final FriendshipState state;
+
+  FriendshipComplete(
+      {this.id,
+      required this.user,
+      required this.otherUser,
+      required this.state});
+}
--- lib/data_access/facade/friendship_facade.dart
+++ lib/data_access/facade/friendship_facade.dart
@@ -1,16 +1,22 @@
+import 'package:book_keeping/common/model/friendship_complete.dart';
 import 'package:book_keeping/common/model/friendship_state.dart';
+import 'package:book_keeping/common/service/converter_service.dart';
 import 'package:book_keeping/data_access/facade/base_facade.dart';
-import 'package:book_keeping/data_access/model/friendship.dart';
 import 'package:book_keeping/data_access/service/friendship_service.dart';
 import 'package:get_it/get_it.dart';
 
 class FriendshipFacade extends BaseFacade {
   final _friendshipService = GetIt.instance.get<FriendshipService>();
+  final _converterService = GetIt.instance.get<ConverterService>();
 
   /// returns stream of friendships where current user equals to either userId or otherUserId fields, not filtered by state
-  Future<Stream<List<Friendship>>> getFriendshipStream() async {
+  Future<Stream<List<FriendshipComplete>>> getFriendshipStream() async {
     final currentUser = await getCurrentUser();
-    return _friendshipService.getStream(currentUser.id!);
+    return _friendshipService.getStream(currentUser.id!).asyncMap(
+        (friendships) => Stream.fromIterable(friendships)
+            .asyncMap(
+                (friendship) => _converterService.fromFriendship(friendship))
+            .toList());
   }
 
   /// creates friendship with sent state with specified otherUserId
