18ca548cc2f51b0fa61e9a7288f0171a394e212a
Jakub Šušlík <suslikjakub@gmail.com>
2024-01-12 18:38:02 +0100

    changed rating value to double, added createOrUpdateMethod to book_rating_facade.dart

--- lib/data_access/service/book_rating_service.dart
+++ lib/data_access/service/book_rating_service.dart
@@ -15,13 +15,22 @@ class BookRatingService extends BaseFirestoreService<BookRating> {
           .where((bookRating) => bookRating.bookId == bookId)
           .toList());
 
-  Future<void> updateRating(String id, int newValue) async {
+  Future<void> updateRating(String id, double newValue, String text) async {
     final rating = await getSingle(id).first;
     if (rating == null) {
       throw Exception(
           "${CollectionType.bookRating.collectionPath} with id: $id does not exist");
     }
-    await update(rating.copyWith(rating: newValue));
+    await update(rating.copyWith(rating: newValue, text: text));
+  }
+
+  Future<BookRating?> getByIds(String userId, String bookId) async {
+    final ratings = await getAll().first;
+    return ratings
+        .where((rating) =>
+            rating ==
+            BookRating(userId: userId, bookId: bookId, text: "", rating: 0.0))
+        .singleOrNull;
   }
 
   static Map<String, dynamic> _toJson(BookRating bookRating) =>
